cc27ace94e8595d7ccc7ec6a80440e1c31c09537
cosmicog <orhangazi4401@gmail.com>
2018-02-25 08:19:56 +0300

    Add coin dashboard support

--- display.sh
+++ display.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 display.py -a PASTE_YOUR_API_KEY_HERE -f TRY -i PASTE_YOUR_ID_HERE
+python3 display.py -a PASTE_YOUR_API_KEY_HERE -f TRY -i PASTE_YOUR_ID_HERE -d PLACE_YOUR_COIN
